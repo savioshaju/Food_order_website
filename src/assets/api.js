10787c14e308506d165c5ef1e2d10c312e9c1568
savioshaju <savioshajum@gmail.com>
2025-07-26 22:44:25 +0530

    corrected api calls

--- src/assets/api.js
+++ src/assets/api.js
@@ -1,16 +1,20 @@
 import axios from "axios";
 
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+});
+
 export const fetchRestaurant = async () => {
-  const response = await axios.get('/api/api/Restaurant');
+  const response = await api.get('/api/Restaurant');
   return response.data;
 };
 
 export const fetchRestaurantMenu = async (restaurantId) => {
-  const response = await axios.get(`/api/api/Restaurant/${restaurantId}/menu`);
+  const response = await api.get(`/api/Restaurant/${restaurantId}/menu`);
   return response.data;
 };
 
 export const fetchallItems = async () => {
-  const response = await axios.get(`/api/api/Restaurant/items`);
+  const response = await api.get(`/api/Restaurant/items`);
   return response.data;
 };
